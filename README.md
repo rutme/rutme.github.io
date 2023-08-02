114ca6b1c58eeafc02ef75eae1729496b320a469
Ruslan Aksenov <akseonowww@ya.ru>
2023-08-02 08:25:16 +0300

    Create project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# rutme.github.io
+# Аналог t.me через GitHub Pages
+
+Просто замените [https://t.me/](https://t.me/) на [https://rutme.github.io/](https://rutme.github.io/)
+
+Должно получиться что-то в стиле [https://rutme.github.io/rkn_tg](https://rutme.github.io/rkn_tg)
+
+Алгоритм для быстрого запоминания:
+
+1. https:// **t.me** /rkn_tgㅤ →ㅤ https:// **rutme** /rkn_tg <br>
+2. https:// **rutme** /rkn_tgㅤ → ㅤhttps:// **rutme.github.io** /rkn_tg
+
+## Преимущества
+
+-  Нет рекламы
+-  Нет трекеров
+-  Открытые исходники https://github.com/tgrm/tgrm.github.io с возможностью поднять сервис на своём сервере.
+
+P.S. Проект является форком репозитория [tgrm.github.io](https://github.com/tgrm/tgrm.github.io)
---
+++ rollup.config.js
@@ -0,0 +1,121 @@
+import autoprefixer from 'autoprefixer';
+import { promisify } from 'util';
+import cssnano from 'cssnano';
+import { promises as fs, readFileSync } from 'fs';
+import postcss from 'postcss';
+import cssImport from 'postcss-import';
+import html from 'rollup-plugin-bundle-html';
+import sass from 'rollup-plugin-sass';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+import uncss from 'uncss';
+import wi from 'web-resource-inliner';
+
+const wri = promisify(wi.html);
+
+const template = `<html><head><meta charset="UTF-8"><link rel="icon" type="image/png" href="data:image/png;base64,${readFileSync('./tg.png').toString('base64')}"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="noindex,nofollow"><title>Telegram</title><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/></head><body><div class="wrapper"><div class="main animate__animated animate__fadeInDown"><h2 id="linkName" class="animate__animated animate__fadeInDown animate__delay-0.5s animate__fast"></h2><div class="animate__animated animate__fadeInDown animate__delay-0.8s animate__fast"> <a id="linkURL" href="#">Открыть</a> </div></div><h1> <a href="https://github.com/rutme/rutme.github.io" target="_blank" class="animate__animated animate__fadeInUp animate__delay-1s">rutme.github.io</a></h1></div></body></html>`;
+
+{/* 
+
+ <div class="main animate__animated animate__fadeInDown">
+        <h2
+          class="animate__animated animate__fadeInDown animate__delay-0.5s animate__fast"
+          id="linkName"
+        >
+          t.me/rkn_tg
+        </h2>
+        <div
+          class="animate__animated animate__fadeInDown animate__delay-0.8s animate__fast"
+        >
+          <a id="linkURL" href="#">Открыть</a>
+        </div>
+      </div>
+      <h1>
+        <a
+          href="https://github.com/rutme/rutme.github.io"
+          target="_blank"
+          class="animate__animated animate__fadeInUp animate__delay-1s"
+          >rutme.github.io</a
+        >
+      </h1>
+
+*/}
+
+// <!DOCTYPE html>
+// <html lang="ru">
+// <head>
+//     <title>Telegram</title>
+//     <meta charset="UTF-8" />
+
+//     <link rel="apple-touch-icon" href="./images/apple-touch-icon.png" />
+//     <!-- 180x180 - ставим первым для safari -->
+//     <link rel="icon" href="./images/favicon.ico" sizes="any" />
+//     <!-- 32x32 -->
+//     <link rel="icon" href="./images/icon.svg" type="image/svg+xml" />
+//     <link rel="manifest" href="./manifest.webmanifest" />
+//     <link rel="yandex-tableau-widget" href="./tableau.json">
+
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <meta name="robots" content="noindex,nofollow" />
+// <body>
+// </body>
+// </html>`;
+
+export default {
+    input: 'index.js',
+    output: {
+        format: 'esm',
+        file: 'dist/bundle.js',
+    },
+    plugins: [
+        sass({
+            output: true,
+            processor: css => postcss(
+                cssImport(),
+                //@ts-ignore
+                autoprefixer(),
+                cssnano({ preset: ['advanced', { discardComments: { removeAll: true } }] }),
+            ).process(css, { from: undefined }).then(c => new Promise((resolve, reject) => (
+                //@ts-ignore
+                uncss(template, { raw: c.css }, (err, output) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(output)
+                    }
+                })
+            )))
+        }),
+        terser({
+            ecma: 5,
+            module: true,
+            toplevel: true,
+            mangle: {
+                reserved: ['location', 'document']
+            }
+        }),
+        html({
+            template,
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        {
+            async writeBundle() {
+                await fs.writeFile('404.html', await wri({
+                    fileContent: readFileSync('./dist/index.html', 'utf-8'),
+                    relativeTo: './dist',
+                    images: true
+                }));
+            }
+        },
+        process.env.ROLLUP_WATCH && serve({
+            open: true,
+            openPage: '/404.html#rkn_tg',
+            contentBase: '',
+            historyApiFallback: false,
+            host: '127.0.0.1',
+            port: 3215,
+        })
+    ]
+};
